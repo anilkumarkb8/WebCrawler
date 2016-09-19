0b2d8dbc73f64e3d59540e1fa80074d50e2f3101
anilkumarkb8 <anilkb888@gmail.com>
2016-09-20 01:01:58 +0530

    Commit 1
    Project Structure Commit

---
+++ src/main/java/com/org/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.org.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/test")
+	public ModelAndView testMessage(
+			@RequestParam(value = "name", required = false) String name) {
+		// Name of your jsp file as parameter
+		ModelAndView view = new ModelAndView("test");
+		view.addObject("name", name);
+		return view;
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+    <h2>Landing Page</h2>
+    <h3><a href="test?name=anil">Click here...</a></h3>
+</body>
+</html>
